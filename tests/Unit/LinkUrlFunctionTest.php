ef0e7100d1c109b33b1f8f6352d2b25ed7d051b2
mouson <mouson@gmail.com>
2017-04-11 23:58:01 +0800

    LinkUrl Step 2 First unit test

--- tests/Unit/LinkUrlFunctionTest.php
+++ tests/Unit/LinkUrlFunctionTest.php
@@ -5,5 +5,17 @@ use Tests\TestCase;
 
 class LinkUrlFunctionTest extends TestCase
 {
-    
+    public function testGeneratesAnchorTag()
+    {
+        /** Arrange */
+
+        /** Assume */
+        $expected = "<a href='http://localhost/dogs/1'>Show Dog</a>";
+
+        /** Act */
+        $actual = link_url('dogs/1', 'Show Dog');
+
+        /** Assert */
+        $this->assertEquals($expected, $actual);
+    }
 }
