b311c5cc4f07a6e8ad506834db8bb3ee11c3758c
mouson <mouson@gmail.com>
2017-04-12 00:28:52 +0800

    LinkUrl Step 7 add new feature: applies attributes array

--- tests/Unit/LinkUrlFunctionTest.php
+++ tests/Unit/LinkUrlFunctionTest.php
@@ -18,4 +18,18 @@ class LinkUrlFunctionTest extends TestCase
         /** Assert */
         $this->assertEquals($expected, $actual);
     }
+
+    public function testAppliesAttributesArray()
+    {
+        /** Arrange */
+
+        /** Assume */
+        $expected = "<a href='http://localhost/dogs/1' class='button'>Show Dog</a>";
+
+        /** Act */
+        $actual = link_url('dogs/1', 'Show Dog', ['class' => 'button']);
+
+        /** Assert */
+        $this->assertEquals($expected, $actual);
+    }
 }
